4b7d6eba5049d1186021087ca334193745cfea33
ALISURE <562282219@qq.com>
2020-09-13 11:43:36 +0800

    Create 5.py

---
+++ 09-13/5.py
@@ -0,0 +1,21 @@
+n = 2
+data = [[5, 4, 0],
+        [6, 4, 1]]
+
+
+def cmp(one, two):
+    if one[1] > two[1]:
+        return 1
+    elif one[1] == two[1]:
+        if one[0] > two[0]:
+            return 1
+        elif one[0] == two[0]:
+            return 0
+        else:
+            return -1
+    else:
+        return -1
+
+data = sorted(data, cmp=cmp, reverse=True)
+
+print " ".join([str(one[-1]) for one in data])
